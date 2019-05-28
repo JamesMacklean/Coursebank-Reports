61c1e1dc46bbede6babd496bd6a5b009a1dd97c9
junfel <junfel@adarnadigital.com>
2019-05-28 18:12:08 +0800

    cleaned some unused things

--- coursebank_reports/urls.py
+++ coursebank_reports/urls.py
@@ -9,10 +9,4 @@ urlpatterns = [
 
     url(r'^reports/courses/', views.course_list_view, name='course-reports'),
     url(r'^reports/course/{}/enrollments/'.format(settings.COURSE_ID_PATTERN), views.enrollment_list_view, name='course-enrollments-reports'),
-
-    url(r'^reports/course/{}/enrollments/user/(?P<id>\d+)$'.format(settings.COURSE_ID_PATTERN), views.UserProfileDetailView.as_view(), name='course-enrollments-user-detail'),
-
-    url(r'^reports/course/{}/enrollments/numbers/$'.format(settings.COURSE_ID_PATTERN), views.num_enrolled_reports, name='num-enrolled-reports'),
-    url(r'^reports/course/{}/enrollments/counts/$'.format(settings.COURSE_ID_PATTERN), views.enrollment_counts_reports, name='enrollment-counts-reports'),
-    url(r'^reports/course/{}/enrollments/users/$'.format(settings.COURSE_ID_PATTERN), views.enrollments_users_reports, name='enrollment-users-reports'),
 ]
