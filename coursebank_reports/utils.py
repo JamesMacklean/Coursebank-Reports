b1df387a302c64fcd3dd185f17546c50f6dfadbc
elkin <elkin@coursebank.ph>
2020-08-13 18:22:59 +0800

    added utils, management command

---
+++ coursebank_reports/utils.py
@@ -0,0 +1,73 @@
+import logging
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+from django.contrib.auth.models import User
+from opaque_keys.edx.keys import CourseKey
+from student.models import UserProfile
+from lms.djangoapps.certificates.api import get_certificate_for_user
+from openedx.core.djangoapps.course_groups.models import CourseUserGroup
+import unicodecsv
+
+LOGGER = logging.getLogger(__name__)
+
+def export_learner_profiles_with_cohort(course_id, email_address=None):
+    """"""
+    tnow = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
+    course_key = CourseKey.from_string(course_id)
+    enrollments = UserProfile.objects.filter(
+        course_id=course_key,
+        is_active=True
+    )
+
+    user_list = []
+    for e in enrollments:
+
+        cert = get_certificate_for_user(e.user.username, course_key)
+        if cert is not None and cert['status'] == "downloadable":
+            date_completed = cert['created'].strftime('%Y-%m-%dT%H:%M:%S.000Z')
+        else:
+            date_completed = None
+
+        try:
+            cohort_groups = e.users.course_groups
+        except:
+            continue
+
+        for g in cohort_groups.CourseUserGroup.all():
+            user_list.append({
+                "name": e.user.name,
+                "email": e.user.email,
+                "date_completed": date_completed,
+                "reg_no": g.name
+            })
+
+    file_name = '/home/ubuntu/tempfiles/export_learner_profiles_with_cohort_{}.csv'.format(tnow)
+    with open(file_name, mode='w') as csv_file:
+        writer = unicodecsv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,  encoding='utf-8')
+        writer.writerow([
+            'Full Name',
+            'Email',
+            'Date of Completion'
+            'Cohort'
+            ])
+
+        for u in credential_list:
+            writer.writerow({
+                u['name'],
+                u['email'],
+                u['date_completed'],
+                u['cohort']
+                })
+
+    if email_address:
+        email = EmailMessage(
+            'Coursebank - Learner Profiles with Cohort',
+            'Attached file of Learner Profiles with Cohort (as of {})'.format(tnow),
+            'no-reply-learner-profiles-with-cohorts@coursebank.ph',
+            [email_address,],
+        )
+        email.attach_file(file_name)
+        email.send()
