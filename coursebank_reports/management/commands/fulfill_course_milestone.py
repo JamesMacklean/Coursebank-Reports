6f3f23070fca4d83c8d816838019128c603055f4
elkin <elkin@coursebank.ph>
2020-09-24 13:53:18 +0800

    edit milestone command

--- coursebank_reports/management/commands/fulfill_course_milestone.py
+++ coursebank_reports/management/commands/fulfill_course_milestone.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '-u',
-            '--user',
+            '--username',
             type=str,
             help='set user to give milestone',
         )
