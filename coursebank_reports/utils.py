108218c9d40854643cfd12f1911df8cbddbc0c78
unknown <tanglao.nath@gmail.com>
2021-06-11 15:51:39 +0800

    added learners demographics and changed created header field to date enrolled

--- coursebank_reports/utils.py
+++ coursebank_reports/utils.py
@@ -91,6 +91,7 @@ def export_learner_profiles(active, course_id, email_address=None):
         for p in profiles:
             try:
                 user_list.append({
+                    "studentid": p.id,
                     "name": p.profile.name,
                     "username": p.username,
                     "email": p.email,
@@ -99,6 +100,7 @@ def export_learner_profiles(active, course_id, email_address=None):
                 })
             except UserProfile.DoesNotExist:
                 user_list.append({
+                    "studentid": p.id,
                     "name": p.username,
                     "username": p.username,
                     "email": p.email,
@@ -109,6 +111,7 @@ def export_learner_profiles(active, course_id, email_address=None):
         with open(file_name, mode='w') as csv_file:
             writer = unicodecsv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,  encoding='utf-8')
             writer.writerow([
+                'Student ID',
                 'Full Name',
                 'Username',
                 'Email',
@@ -118,6 +121,7 @@ def export_learner_profiles(active, course_id, email_address=None):
 
             for u in user_list:
                 writer.writerow([
+                    u['studentid'],
                     u['name'],
                     u['username'],
                     u['email'],
@@ -150,6 +154,7 @@ def export_learner_profiles(active, course_id, email_address=None):
                 date_completed = ""
 
             user_list.append({
+                "studentid": e.user.id,
                 "name": e.user.profile.name,
                 "username": e.user.username,
                 "email": e.user.email,
@@ -161,22 +166,150 @@ def export_learner_profiles(active, course_id, email_address=None):
         with open(file_name, mode='w') as csv_file:
             writer = unicodecsv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,  encoding='utf-8')
             writer.writerow([
+                'Student ID',
                 'Full Name',
                 'Username',
                 'Email',
-                'Created',
+                'Date Enrolled',
                 'Last Login',
                 'Date Completed',
                 ])
 
             for u in user_list:
                 writer.writerow([
+                    u['studentid'],
                     u['name'],
                     u['username'],
                     u['email'],
                     u['created'],
                     u['last_login'],
                     u['date_completed']
+                    ])
+
+        if email_address:
+            email = EmailMessage(
+                'Coursebank - Learner Profiles',
+                'Attached file of Learner Profiles (as of {})'.format(tnow),
+                'no-reply-learner-profiles@coursebank.ph',
+                [email_address,],
+            )
+            email.attach_file(file_name)
+            email.send()
+
+def export_learner_demographics(active, course_id, email_address=None):
+    """"""
+    tnow = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
+    user_list = []
+
+    if course_id is None:
+        if active == "yes":
+          profiles = User.objects.filter(is_active=True)
+        else:
+          profiles = User.objects.all()
+
+        for p in profiles:
+            try:
+                user_list.append({
+                    "studentid": p.id,
+                    "name": p.profile.name,
+                    "username": p.username,
+                    "email": p.email,
+                    "gender": UserProfile.objects.filter(user_id=p.id).values_list('gender').get(),
+                    "birthyear": UserProfile.objects.filter(user_id=p.id).values_list('year_of_birth').get(),
+                    "location": UserProfile.objects.filter(user_id=p.id).values_list('country').get(),
+                })
+            except UserProfile.DoesNotExist:
+                user_list.append({
+                    "studentid": p.id,
+                    "name": p.username,
+                    "username": p.username,
+                    "email": p.email,
+                    "gender": UserProfile.objects.filter(user_id=p.id).values_list('gender').get(),
+                    "birthyear": UserProfile.objects.filter(user_id=p.id).values_list('year_of_birth').get(),
+                    "location": UserProfile.objects.filter(user_id=p.id).values_list('country').get(),
+                })
+        file_name = '/home/ubuntu/tempfiles/export_learner_profiles_{}.csv'.format(tnow)
+        with open(file_name, mode='w') as csv_file:
+            writer = unicodecsv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,  encoding='utf-8')
+            writer.writerow([
+                'Student ID',
+                'Full Name',
+                'Username',
+                'Email',
+                'Gender',
+                'Birth Year',
+                'Location',
+                ])
+
+            for u in user_list:
+                writer.writerow([
+                    u['studentid']
+                    u['name'],
+                    u['username'],
+                    u['email'],
+                    u['gender'],
+                    u['birthyear'],
+                    u['location'],
+                    ])
+
+        if email_address:
+            email = EmailMessage(
+                'Coursebank - Learner Profiles',
+                'Attached file of Learner Profiles (as of {})'.format(tnow),
+                'no-reply-learner-profiles@coursebank.ph',
+                [email_address,],
+            )
+            email.attach_file(file_name)
+            email.send()
+
+    else:
+        course_key = CourseKey.from_string(course_id)
+        enrollments = CourseEnrollment.objects.filter(
+            course_id=course_key,
+            is_active=True
+        )
+
+        for e in enrollments:
+            cert = get_certificate_for_user(e.user.username, course_key)
+            if cert is not None and cert['status'] == "downloadable":
+                date_completed = cert['created'].strftime('%Y-%m-%dT%H:%M:%S.000Z')
+            else:
+                date_completed = ""
+
+            user_list.append({
+                "name": e.user.profile.name,
+                "username": e.user.username,
+                "email": e.user.email,
+                "gender": UserProfile.objects.filter(user_id=e.user.id).values_list('gender').get(),
+                "birthyear": UserProfile.objects.filter(user_id=e.user.id).values_list('year_of_birth').get(),
+                "location": UserProfile.objects.filter(user_id=e.user.id).values_list('country').get(),
+                "created": e.created,
+            })
+        file_name = '/home/ubuntu/tempfiles/export_learner_profiles_{}.csv'.format(tnow)
+        with open(file_name, mode='w') as csv_file:
+            writer = unicodecsv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL,  encoding='utf-8')
+            writer.writerow([
+                'Student ID',
+                'Full Name',
+                'Username',
+                'Email',
+                'Gender',
+                'Birth Year',
+                'Location',
+                'Date Enrolled',
+                ])
+
+            for u in user_list:
+                writer.writerow([
+                    u['studentid']
+                    u['name'],
+                    u['username'],
+                    u['email'],
+                    u['gender'],
+                    u['birthyear'],
+                    u['location'],
+                    u['created'],
                     ])
 
         if email_address:
